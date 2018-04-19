68664725e8a30f36bb5b4481a74e7174a358ec61
pchansen <phansen@fivedirections.com>
2018-04-19 19:21:31 -0400

    diff protocol

--- server/server.rb
+++ server/server.rb
@@ -5,30 +5,29 @@ require "digest"
 
 class Servlet < WEBrick::HTTPServlet::AbstractServlet
     @@prng = Random.new
-
     @@volts = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
     @@hist = Hash.new
+    @@change = Hash.new
+    @@on = false
 
     def do_GET (request, response)
-        puts request.query_string
+        if !@@on then
+            start_detection()
+            @@on = true
+        end
+
         response.status = 200
         response['Content-Type'] = 'text/plain'
         response.body = ""
         i = 0
-        @@volts.each_with_index { |prev, i|
-            if prev <= 10 then
-                @@volts[i] += @@prng.rand(-(prev)..10)
-            elsif prev >= 90 then
-                @@volts[i] += @@prng.rand(-10..(100-prev))
-            else 
-                @@volts[i] += @@prng.rand(-10..10)
-            end
-            response.body += "&#{i}=#{@@volts[i]}"
-        }
+        @@change.each do |key, val|
+            response.body += "&#{key}=#{val}"
+        end
+        @@change = Hash.new
         sha256 = Digest::SHA256.base64digest response.body
         @@hist[sha256] = @@volts
         response.body = response.body + "&" + sha256
+        puts response.body
         # request.query_string.split("&").each { |couplet| 
         # 	vararray = couplet.split("=");
         # 	if vararray[0] == "data" then
@@ -37,16 +36,38 @@ class Servlet < WEBrick::HTTPServlet::AbstractServlet
         # 	end
         # }
     end
+
+    def start_detection()
+        Thread.new do 
+            while true do   
+                i = 0
+                @@volts.each_with_index { |prev, i|
+                    if prev <= 4 then
+                        @@volts[i] += @@prng.rand(-(prev)..4)
+                        @@change[i] = @@volts[i]
+                    elsif prev >= 96 then
+                        @@volts[i] += @@prng.rand(-4..(100-prev))
+                        @@change[i] = @@volts[i]
+                    else 
+                        d = @@prng.rand(-4..4)
+                        @@volts[i] += d
+                        if d != 0 then
+                            @@change[i] = @@volts[i]
+                        end 
+                    end
+                }
+                sleep 0.75
+            end
+        end
+        return
+    end
+
 end
 
 prng = Random.new
-
 volts = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
 hist = Hash.new
-
 server = WEBrick::HTTPServer.new(:Port => 4000)
-
 server.mount "/", Servlet
 
 trap("INT") {
@@ -54,3 +75,4 @@ trap("INT") {
 }
 
 server.start
+
